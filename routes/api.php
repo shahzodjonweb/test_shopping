743689e360df42267329ced199cf18ebda2038de
shahzodjonweb <shahzodprogramma@gmail.com>
2021-10-30 23:55:53 +0500

    routes added and other small changes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\ProductsController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::group([
+    'middleware' => 'sanctum',
+    'prefix' => 'auth'
+
+], function ($router) {
+    //There should be login details
+    // Route::post('/login', [DriverAuthController::class, 'login']);
+    // Route::post('/logout', [DriverAuthController::class, 'logout']);
+    // Route::post('/refresh', [DriverAuthController::class, 'refresh']);
+    // Route::get('/user-profile', [DriverAuthController::class, 'userProfile']);     
+});
+
+Route::group([
+    'middleware' => 'sanctum',
+    'prefix' => 'details'
+], function ($router) {
+    Route::resource('category', CategoriesController::class);
+    Route::resource('product', ProductsController::class);
+    // Route::post('/login', [DriverAuthController::class, 'login']);
+    // Route::post('/logout', [DriverAuthController::class, 'logout']);
+    // Route::post('/refresh', [DriverAuthController::class, 'refresh']);
+    // Route::get('/user-profile', [DriverAuthController::class, 'userProfile']);     
 });
